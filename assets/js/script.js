7173e4c7c6f60fe63ee28f5528edcaed16608b64
fredazure <fredrik@epigon.se>
2025-06-06 17:36:01 +0200

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("blog.json")
+        .then(response => response.json())
+        .then(data => {
+            const blogContainer = document.getElementById("blog-posts");
+            data.forEach(post => {
+                let postElement = document.createElement("div");
+                postElement.innerHTML = `<h2>${post.title}</h2>
+                                         <p><em>${post.date}</em></p>
+                                         <p>${post.content}</p>`;
+                blogContainer.appendChild(postElement);
+            });
+        })
+        .catch(error => console.error("Fel vid inläsning av bloggen:", error));
+});
